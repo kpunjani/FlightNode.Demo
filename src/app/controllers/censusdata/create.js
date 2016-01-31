627105c476a74babc903234597084ea2db42dafa
kpunjani <kpunjani@gmail.com>
2016-01-31 23:19:06 +0100

    Converted checkboxes into radio button. Added all the lookup array into a single data property. Bind fields to ng-model, few are still remaining

--- src/app/controllers/censusdata/create.js
+++ src/app/controllers/censusdata/create.js
@@ -12,16 +12,18 @@ angular.module('flightNodeApp')
     ['$scope',
     function ($scope) {
 		$scope.loading = true;
-      
-        //TODO: Get all this data from DB through API calls. Since it is all lookup data, we may cache it through out the application's lifetime.
-        $scope.tideInfo = [
+        $scope.data = {};
+        $scope.data.mydata='hi';
+        //TODO: Get all this data from DB through API calls. 
+        //Since it is all lookup data, we may cache it when first time users comes on this page.
+        $scope.data.tideInfo = [
             { Id: 1, Description: "Water level appears high" },
             { Id: 2, Description: "Water level appears low" },
             { Id: 3, Description: "Wind driven" },
             { Id: 4, Description: "Non-tidal" },      
         ];
         
-        $scope.weatherInfo = [
+        $scope.data.weatherInfo = [
             { Id: 1, Description: "Clear"},
             { Id: 2, Description: "Partly cloudy"},
             { Id: 3, Description: "Overcast"},
@@ -30,18 +32,18 @@ angular.module('flightNodeApp')
             { Id: 6, Description: "Showers"},
         ];
         
-        $scope.vantagePointInfo = [
+        $scope.data.vantagePointInfo = [
             { Id: 1, Description: "On-Site Visit"},
             { Id: 2, Description: "View from adj area by vehicle/boat/on foot"},
         ];
         
-        $scope.accessPointInfo = [
+        $scope.data.accessPointInfo = [
             { Id: 1, Description: "On Foot"},
             { Id: 2, Description: "Vessel (motor)"},
             { Id: 3, Description: "Kayak/ Canoe"}
         ];
         
-        $scope.siteTypeInfo = [
+        $scope.data.siteTypeInfo = [
             { Id: 1, Description: "New Colony"},
             { Id: 2, Description: "Surveyed, Inactive"},
             { Id: 3, Description: "Prev. Known Colony (Count only)"},
@@ -49,13 +51,13 @@ angular.module('flightNodeApp')
             { Id: 5, Description: "Prev. Known Colony (Count and Description Update)"},
         ];
         
-        $scope.feedingRateInfo = [
+        $scope.data.feedingRateInfo = [
             { Id: 1, Description: "Low Success"},
             { Id: 2, Description: "Medium Success"},
             { Id: 3, Description: "High Success"},
         ];
         
-        $scope.habitatInfo = [
+        $scope.data.habitatInfo = [
             { Id: 1, Description: "Open Water Below Knee"},
             { Id: 2, Description: "Open Water Above Knee"},
             { Id: 3, Description: "Manmade (ditch, culvert, etc..)"},        
