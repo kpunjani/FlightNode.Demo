a3d3815597dbe2128aa04d2383f77680403362f8
Stephen Fuqua <stephen.fuqua@trintech.com>
2016-03-24 22:15:16 -0500

    Broken ui-bootstrap fixed. Merged two namespaces in census create in order to avoid namespace conflict.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -22,6 +22,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
@@ -32,11 +33,13 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-ui-grid/ui-grid.js',
       'bower_components/moment/moment.js',
+      'bower_components/lodash/lodash.js',
       'bower_components/requirejs/require.js',
       'bower_components/angular-jwt/dist/angular-jwt.js',
       'bower_components/ui-navbar/release/js/ui-navbar.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/ng-csv/build/ng-csv.min.js',
+      'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
